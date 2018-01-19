b01c69178da89c521c1d04cd744cfa5a08ac5044
Brad Hambrice <hambrice@uab.edu>
2018-01-19 07:32:24 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,6 +3,6 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy"
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 5, "F", "black", "sneezy", "T")
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 9, "F", "brown", "doc", "T")
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 2, "F", "white", "grumpy", "T")
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 7, "M", "black", "", "T")
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "brown", "hungry", "T")
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 7, "M", "black", "happy", "T")
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 4, "F", "brown", "bashful", "T")
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 4, "M", "brown", "hungry", "T")
